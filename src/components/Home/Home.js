ab7db1845468a37b4e2cda527d4c9e93625c22fe
Lewis Clarke <lewisclarke467@gmail.com>
2024-03-17 13:53:51 +0000

    Building layout and home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="section flex flex-col justify-center items-end">
+      <div className="hero w-1/2 h-1/2 text-right bg-[#1E7964] p-2 rounded-l-lg">
+        <div className="title">
+          <h1 className="text-6xl">Lewis Clarke</h1>
+          <h2 className="text-2xl">Front End Developer</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <button>Home</button>
+      <button>Projects</button>
+      <button>About my</button>
+    </nav>
+  );
+};
+
+export default Nav;
